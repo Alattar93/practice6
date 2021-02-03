afecd0d51ab67a6746d5ce6e0d4c8d9fdd80cff5
Mohamed Alattar <mnowzad@gmail.com>
2021-02-02 20:31:40 -0800

    adding work2 to Branch2

--- src/test/java/Branch2.java
+++ src/test/java/Branch2.java
@@ -2,5 +2,6 @@ public class Branch2 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello2");
+        System.out.println("hello3");
     }
 }
